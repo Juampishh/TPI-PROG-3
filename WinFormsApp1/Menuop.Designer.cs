1f3b6a276b2f97a36d546944d64cf0cd7eb43f32
sanchezchandialf <lautarosanche@gmail.com>
2024-06-22 19:34:39 -0300

    solucion del problema buttom1, diseño de menuop etc

--- WinFormsApp1/Menuop.Designer.cs
+++ WinFormsApp1/Menuop.Designer.cs
@@ -17,14 +17,15 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menuop));
             panelEdicion = new Panel();
+            Edicion = new Button();
             btnEdicion = new Button();
             labelEdicion = new Label();
             panelPrevisualizacion = new Panel();
             ingresoPrev = new Button();
             btnPrevisualizacion = new Button();
             labelPrevisualizacion = new Label();
-            Edicion = new Button();
             panelEdicion.SuspendLayout();
             panelPrevisualizacion.SuspendLayout();
             SuspendLayout();
@@ -42,6 +43,18 @@
             panelEdicion.MouseEnter += panelEdicion_MouseEnter;
             panelEdicion.MouseLeave += panelEdicion_MouseLeave;
             // 
+            // Edicion
+            // 
+            Edicion.BackgroundImage = (Image)resources.GetObject("Edicion.BackgroundImage");
+            Edicion.BackgroundImageLayout = ImageLayout.Stretch;
+            Edicion.FlatStyle = FlatStyle.Popup;
+            Edicion.Location = new Point(0, 0);
+            Edicion.Name = "Edicion";
+            Edicion.Size = new Size(300, 450);
+            Edicion.TabIndex = 0;
+            Edicion.UseVisualStyleBackColor = true;
+            Edicion.Click += Edicion_Click;
+            // 
             // btnEdicion
             // 
             btnEdicion.BackColor = Color.DodgerBlue;
@@ -58,10 +71,11 @@
             // labelEdicion
             // 
             labelEdicion.AutoSize = true;
-            labelEdicion.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelEdicion.Font = new Font("Century Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelEdicion.ForeColor = SystemColors.Control;
             labelEdicion.Location = new Point(90, 480);
             labelEdicion.Name = "labelEdicion";
-            labelEdicion.Size = new Size(121, 41);
+            labelEdicion.Size = new Size(123, 37);
             labelEdicion.TabIndex = 1;
             labelEdicion.Text = "Edición";
             // 
@@ -80,6 +94,9 @@
             // 
             // ingresoPrev
             // 
+            ingresoPrev.BackgroundImage = (Image)resources.GetObject("ingresoPrev.BackgroundImage");
+            ingresoPrev.BackgroundImageLayout = ImageLayout.Stretch;
+            ingresoPrev.FlatStyle = FlatStyle.Popup;
             ingresoPrev.Location = new Point(0, 0);
             ingresoPrev.Name = "ingresoPrev";
             ingresoPrev.Size = new Size(300, 450);
@@ -103,27 +120,19 @@
             // labelPrevisualizacion
             // 
             labelPrevisualizacion.AutoSize = true;
-            labelPrevisualizacion.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelPrevisualizacion.Font = new Font("Century Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelPrevisualizacion.ForeColor = SystemColors.Control;
             labelPrevisualizacion.Location = new Point(370, 480);
             labelPrevisualizacion.Name = "labelPrevisualizacion";
-            labelPrevisualizacion.Size = new Size(243, 41);
+            labelPrevisualizacion.Size = new Size(249, 37);
             labelPrevisualizacion.TabIndex = 3;
             labelPrevisualizacion.Text = "Previsualización";
-            // 
-            // Edicion
-            // 
-            Edicion.Location = new Point(0, 0);
-            Edicion.Name = "Edicion";
-            Edicion.Size = new Size(300, 450);
-            Edicion.TabIndex = 0;
-            Edicion.UseVisualStyleBackColor = true;
-            Edicion.Click += Edicion_Click;
             // 
             // Menuop
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.White;
+            BackColor = Color.DarkRed;
             ClientSize = new Size(660, 540);
             Controls.Add(labelPrevisualizacion);
             Controls.Add(panelPrevisualizacion);
@@ -131,6 +140,7 @@
             Controls.Add(panelEdicion);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "Menuop";
+            Opacity = 0.9D;
             Text = "Menú de Opciones";
             Load += Menuop_Load;
             panelEdicion.ResumeLayout(false);
--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+406fbf93cc583ab8686964ab76d44296233522d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea5a39e2d1a20bc94c9bc5207271efba908681d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
