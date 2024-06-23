f5c694393511cec3bb16433aa5ccd7aabaeed51a
TomasBurgos123 <tburgos400@gmail.com>
2024-06-23 13:29:00 -0300

    Boton de elegir mesa terminado, diferentes tipos de mesas terminado, se agrego funcionalidades con el click derecho a las mesas y se separo las clases de las mesas en una carpeta aparte

--- WinFormsApp1/ElegirMesa.Designer.cs
+++ WinFormsApp1/ElegirMesa.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ElegirMesa";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ElegirMesa));
+            label1 = new Label();
+            PBCuatro = new PictureBox();
+            label2 = new Label();
+            PBDos = new PictureBox();
+            PBSeis = new PictureBox();
+            label3 = new Label();
+            label4 = new Label();
+            BTCancelar = new Button();
+            ((System.ComponentModel.ISupportInitialize)PBCuatro).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PBDos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PBSeis).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Franklin Gothic Book", 12F, FontStyle.Underline, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Agregar mesa para:";
+            // 
+            // PBCuatro
+            // 
+            PBCuatro.BackgroundImageLayout = ImageLayout.None;
+            PBCuatro.BorderStyle = BorderStyle.Fixed3D;
+            PBCuatro.Image = (Image)resources.GetObject("PBCuatro.Image");
+            PBCuatro.Location = new Point(196, 86);
+            PBCuatro.Name = "PBCuatro";
+            PBCuatro.Size = new Size(114, 59);
+            PBCuatro.SizeMode = PictureBoxSizeMode.Zoom;
+            PBCuatro.TabIndex = 1;
+            PBCuatro.TabStop = false;
+            PBCuatro.Click += pbCuatroP_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(205, 148);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Cuatro personas";
+            // 
+            // PBDos
+            // 
+            PBDos.BackgroundImageLayout = ImageLayout.None;
+            PBDos.BorderStyle = BorderStyle.Fixed3D;
+            PBDos.Image = (Image)resources.GetObject("PBDos.Image");
+            PBDos.Location = new Point(12, 86);
+            PBDos.Name = "PBDos";
+            PBDos.Size = new Size(114, 59);
+            PBDos.SizeMode = PictureBoxSizeMode.Zoom;
+            PBDos.TabIndex = 3;
+            PBDos.TabStop = false;
+            PBDos.Click += PBDos_Click;
+            // 
+            // PBSeis
+            // 
+            PBSeis.BackgroundImageLayout = ImageLayout.None;
+            PBSeis.BorderStyle = BorderStyle.Fixed3D;
+            PBSeis.Image = (Image)resources.GetObject("PBSeis.Image");
+            PBSeis.Location = new Point(373, 86);
+            PBSeis.Name = "PBSeis";
+            PBSeis.Size = new Size(114, 59);
+            PBSeis.SizeMode = PictureBoxSizeMode.Zoom;
+            PBSeis.TabIndex = 4;
+            PBSeis.TabStop = false;
+            PBSeis.Click += PBSeis_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(32, 148);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Dos personas";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(389, 148);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Seis personas";
+            // 
+            // BTCancelar
+            // 
+            BTCancelar.Location = new Point(391, 208);
+            BTCancelar.Name = "BTCancelar";
+            BTCancelar.Size = new Size(75, 23);
+            BTCancelar.TabIndex = 7;
+            BTCancelar.Text = "Cancelar";
+            BTCancelar.UseVisualStyleBackColor = true;
+            BTCancelar.Click += BTCancelar_Click;
+            // 
+            // ElegirMesa
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(499, 243);
+            Controls.Add(BTCancelar);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(PBSeis);
+            Controls.Add(PBDos);
+            Controls.Add(label2);
+            Controls.Add(PBCuatro);
+            Controls.Add(label1);
+            Name = "ElegirMesa";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ElegirMesa";
+            ((System.ComponentModel.ISupportInitialize)PBCuatro).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PBDos).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PBSeis).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private PictureBox PBCuatro;
+        private Label label2;
+        private PictureBox PBDos;
+        private PictureBox PBSeis;
+        private Label label3;
+        private Label label4;
+        private Button BTCancelar;
     }
 }
--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panel1 = new Panel();
+            pictureBox2 = new PictureBox();
             btnAddPared = new Button();
             agregarMesaButton = new Button();
             panel2 = new Panel();
@@ -38,35 +39,38 @@
             button2 = new Button();
             btnModificar = new Button();
             panel3 = new Panel();
-            pictureBox2 = new PictureBox();
-            button7 = new Button();
-            button6 = new Button();
-            button5 = new Button();
-            button1 = new Button();
             panel1.SuspendLayout();
-            panel2.SuspendLayout();
-            panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.DarkGray;
+            panel1.Controls.Add(pictureBox2);
             panel1.Controls.Add(btnAddPared);
             panel1.Controls.Add(agregarMesaButton);
             panel1.Dock = DockStyle.Bottom;
-            panel1.Location = new Point(0, 775);
-            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Location = new Point(0, 462);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1152, 133);
+            panel1.Size = new Size(1008, 100);
             panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Dock = DockStyle.Right;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(891, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(117, 100);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
             // 
             // btnAddPared
             // 
-            btnAddPared.Location = new Point(183, 23);
-            btnAddPared.Margin = new Padding(3, 4, 3, 4);
+            btnAddPared.Location = new Point(160, 17);
             btnAddPared.Name = "btnAddPared";
-            btnAddPared.Size = new Size(152, 60);
+            btnAddPared.Size = new Size(133, 45);
             btnAddPared.TabIndex = 4;
             btnAddPared.Text = "Agregar Pared";
             btnAddPared.UseVisualStyleBackColor = true;
@@ -74,10 +78,9 @@
             // 
             // agregarMesaButton
             // 
-            agregarMesaButton.Location = new Point(14, 23);
-            agregarMesaButton.Margin = new Padding(3, 4, 3, 4);
+            agregarMesaButton.Location = new Point(12, 17);
             agregarMesaButton.Name = "agregarMesaButton";
-            agregarMesaButton.Size = new Size(151, 60);
+            agregarMesaButton.Size = new Size(132, 45);
             agregarMesaButton.TabIndex = 3;
             agregarMesaButton.Text = "Agregar Mesa";
             agregarMesaButton.UseVisualStyleBackColor = true;
@@ -90,17 +93,17 @@
             panel2.Controls.Add(button2);
             panel2.Controls.Add(btnModificar);
             panel2.Dock = DockStyle.Right;
-            panel2.Location = new Point(1015, 0);
-            panel2.Margin = new Padding(3, 4, 3, 4);
+            panel2.Location = new Point(888, 0);
             panel2.Name = "panel2";
-            panel2.Size = new Size(137, 775);
+            panel2.Size = new Size(120, 462);
             panel2.TabIndex = 1;
             // 
             // VolverButtom
             // 
-            VolverButtom.Location = new Point(7, 126);
+            VolverButtom.Location = new Point(6, 94);
+            VolverButtom.Margin = new Padding(3, 2, 3, 2);
             VolverButtom.Name = "VolverButtom";
-            VolverButtom.Size = new Size(127, 29);
+            VolverButtom.Size = new Size(111, 22);
             VolverButtom.TabIndex = 3;
             VolverButtom.Text = "Volver";
             VolverButtom.UseVisualStyleBackColor = true;
@@ -111,10 +114,9 @@
             btnSalir.BackColor = Color.Red;
             btnSalir.BackgroundImageLayout = ImageLayout.Center;
             btnSalir.Cursor = Cursors.Hand;
-            btnSalir.Location = new Point(25, 728);
-            btnSalir.Margin = new Padding(3, 4, 3, 4);
+            btnSalir.Location = new Point(22, 546);
             btnSalir.Name = "btnSalir";
-            btnSalir.Size = new Size(88, 39);
+            btnSalir.Size = new Size(77, 29);
             btnSalir.TabIndex = 2;
             btnSalir.Text = "Salir";
             btnSalir.UseVisualStyleBackColor = false;
@@ -122,20 +124,18 @@
             // 
             // button2
             // 
-            button2.Location = new Point(7, 71);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(6, 53);
             button2.Name = "button2";
-            button2.Size = new Size(127, 31);
+            button2.Size = new Size(111, 23);
             button2.TabIndex = 1;
             button2.Text = "Cancelar reservas";
             button2.UseVisualStyleBackColor = true;
             // 
             // btnModificar
             // 
-            btnModificar.Location = new Point(3, 16);
-            btnModificar.Margin = new Padding(3, 4, 3, 4);
+            btnModificar.Location = new Point(3, 12);
             btnModificar.Name = "btnModificar";
-            btnModificar.Size = new Size(127, 31);
+            btnModificar.Size = new Size(111, 23);
             btnModificar.TabIndex = 0;
             btnModificar.Text = "Modificar";
             btnModificar.UseVisualStyleBackColor = true;
@@ -145,92 +145,27 @@
             // 
             panel3.BackColor = Color.Bisque;
             panel3.BorderStyle = BorderStyle.FixedSingle;
-            panel3.Controls.Add(pictureBox2);
-            panel3.Controls.Add(button7);
-            panel3.Controls.Add(button6);
-            panel3.Controls.Add(button5);
-            panel3.Controls.Add(button1);
             panel3.Dock = DockStyle.Fill;
             panel3.Location = new Point(0, 0);
-            panel3.Margin = new Padding(3, 4, 3, 4);
             panel3.Name = "panel3";
-            panel3.Size = new Size(1015, 775);
+            panel3.Size = new Size(888, 462);
             panel3.TabIndex = 2;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(297, 69);
-            pictureBox2.Margin = new Padding(3, 4, 3, 4);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(90, 67);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 4;
-            pictureBox2.TabStop = false;
-            pictureBox2.Click += pictureBox2_Click;
-            // 
-            // button7
-            // 
-            button7.BackgroundImage = (Image)resources.GetObject("button7.BackgroundImage");
-            button7.BackgroundImageLayout = ImageLayout.Zoom;
-            button7.Location = new Point(130, 517);
-            button7.Margin = new Padding(3, 4, 3, 4);
-            button7.Name = "button7";
-            button7.Size = new Size(128, 85);
-            button7.TabIndex = 3;
-            button7.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            button6.BackgroundImage = (Image)resources.GetObject("button6.BackgroundImage");
-            button6.BackgroundImageLayout = ImageLayout.Zoom;
-            button6.Location = new Point(130, 376);
-            button6.Margin = new Padding(3, 4, 3, 4);
-            button6.Name = "button6";
-            button6.Size = new Size(128, 85);
-            button6.TabIndex = 2;
-            button6.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            button5.BackgroundImage = (Image)resources.GetObject("button5.BackgroundImage");
-            button5.BackgroundImageLayout = ImageLayout.Zoom;
-            button5.Location = new Point(130, 225);
-            button5.Margin = new Padding(3, 4, 3, 4);
-            button5.Name = "button5";
-            button5.Size = new Size(128, 85);
-            button5.TabIndex = 1;
-            button5.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.Transparent;
-            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
-            button1.BackgroundImageLayout = ImageLayout.Zoom;
-            button1.Location = new Point(130, 69);
-            button1.Margin = new Padding(3, 4, 3, 4);
-            button1.Name = "button1";
-            button1.Size = new Size(128, 85);
-            button1.TabIndex = 0;
-            button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1152, 908);
+            ClientSize = new Size(1008, 562);
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             WindowState = FormWindowState.Maximized;
             Load += Form1_Load;
             panel1.ResumeLayout(false);
-            panel2.ResumeLayout(false);
-            panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -242,14 +177,10 @@
         private Button btnSalir;
         private Button button2;
         private Button btnModificar;
-        private Button button7;
-        private Button button6;
-        private Button button5;
-        private Button button1;
         private Button agregarMesaButton;
-        private PictureBox pictureBox2;
         private PictureBox pictureBox1;
         private Button btnAddPared;
         private Button VolverButtom;
+        private PictureBox pictureBox2;
     }
 }
