83103ea8ffcef873708285dbeb3c7a4fbdfac2dc
sanchezchandialf <lautarosanche@gmail.com>
2024-06-23 18:37:16 -0300

    agregado de imagenes

--- WinFormsApp1/Menuop.Designer.cs
+++ WinFormsApp1/Menuop.Designer.cs
@@ -17,14 +17,15 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menuop));
             panelEdicion = new Panel();
+            Edicion = new Button();
             btnEdicion = new Button();
             labelEdicion = new Label();
             panelPrevisualizacion = new Panel();
             ingresoPrev = new Button();
             btnPrevisualizacion = new Button();
             labelPrevisualizacion = new Label();
-            Edicion = new Button();
             panelEdicion.SuspendLayout();
             panelPrevisualizacion.SuspendLayout();
             SuspendLayout();
@@ -42,6 +43,17 @@
             panelEdicion.MouseEnter += panelEdicion_MouseEnter;
             panelEdicion.MouseLeave += panelEdicion_MouseLeave;
             // 
+            // Edicion
+            // 
+            Edicion.FlatStyle = FlatStyle.Popup;
+            Edicion.Image = (Image)resources.GetObject("Edicion.Image");
+            Edicion.Location = new Point(0, 0);
+            Edicion.Name = "Edicion";
+            Edicion.Size = new Size(300, 450);
+            Edicion.TabIndex = 0;
+            Edicion.UseVisualStyleBackColor = true;
+            Edicion.Click += Edicion_Click;
+            // 
             // btnEdicion
             // 
             btnEdicion.BackColor = Color.DodgerBlue;
@@ -58,12 +70,10 @@
             // labelEdicion
             // 
             labelEdicion.AutoSize = true;
-
             labelEdicion.Font = new Font("Century Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-
             labelEdicion.Location = new Point(90, 480);
             labelEdicion.Name = "labelEdicion";
-            labelEdicion.Size = new Size(121, 41);
+            labelEdicion.Size = new Size(123, 37);
             labelEdicion.TabIndex = 1;
             labelEdicion.Text = "Edición";
             // 
@@ -82,6 +92,8 @@
             // 
             // ingresoPrev
             // 
+            ingresoPrev.FlatStyle = FlatStyle.Popup;
+            ingresoPrev.Image = (Image)resources.GetObject("ingresoPrev.Image");
             ingresoPrev.Location = new Point(0, 0);
             ingresoPrev.Name = "ingresoPrev";
             ingresoPrev.Size = new Size(300, 450);
@@ -105,37 +117,26 @@
             // labelPrevisualizacion
             // 
             labelPrevisualizacion.AutoSize = true;
-
-            labelPrevisualizacion.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-
+            labelPrevisualizacion.Font = new Font("Century Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             labelPrevisualizacion.Location = new Point(370, 480);
             labelPrevisualizacion.Name = "labelPrevisualizacion";
-            labelPrevisualizacion.Size = new Size(243, 41);
+            labelPrevisualizacion.Size = new Size(249, 37);
             labelPrevisualizacion.TabIndex = 3;
             labelPrevisualizacion.Text = "Previsualización";
-            // 
-            // Edicion
-            // 
-            Edicion.Location = new Point(0, 0);
-            Edicion.Name = "Edicion";
-            Edicion.Size = new Size(300, 450);
-            Edicion.TabIndex = 0;
-            Edicion.UseVisualStyleBackColor = true;
-            Edicion.Click += Edicion_Click;
             // 
             // Menuop
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.White;
-            ClientSize = new Size(660, 540);
-
+            BackColor = Color.Firebrick;
+            ClientSize = new Size(672, 540);
             Controls.Add(labelPrevisualizacion);
             Controls.Add(panelPrevisualizacion);
             Controls.Add(labelEdicion);
             Controls.Add(panelEdicion);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "Menuop";
+            Opacity = 0.9D;
             Text = "Menú de Opciones";
             Load += Menuop_Load;
             panelEdicion.ResumeLayout(false);
