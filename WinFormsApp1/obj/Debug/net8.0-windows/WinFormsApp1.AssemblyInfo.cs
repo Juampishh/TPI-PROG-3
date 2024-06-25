b9442d588f5a4b11deedbfb280d0618a597217b4
sanchezchandialf <lautarosanche@gmail.com>
2024-06-25 18:37:07 -0300

    contraseña no visible

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290a274a8b060295510d9ea416003a389f4c8319")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7070cb7b4df7dcff4b2526ed6eccc59db4b6f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
