05b2353c009befae8e545cffb5110f22c2cec874
sanchezchandialf <lautarosanche@gmail.com>
2024-06-26 18:12:39 -0300

    cambios menu cambiocontraseña

--- WinFormsApp1/CambioContraseña.Designer.cs
+++ WinFormsApp1/CambioContraseña.Designer.cs
@@ -28,44 +28,49 @@
             // txtUsuario
             // 
             txtUsuario.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtUsuario.Location = new Point(44, 46);
+            txtUsuario.Location = new Point(50, 61);
+            txtUsuario.Margin = new Padding(3, 4, 3, 4);
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(263, 27);
+            txtUsuario.Size = new Size(300, 32);
             txtUsuario.TabIndex = 0;
             // 
             // txtContraseñaActual
             // 
             txtContraseñaActual.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtContraseñaActual.Location = new Point(44, 94);
+            txtContraseñaActual.Location = new Point(50, 125);
+            txtContraseñaActual.Margin = new Padding(3, 4, 3, 4);
             txtContraseñaActual.Name = "txtContraseñaActual";
-            txtContraseñaActual.Size = new Size(263, 27);
+            txtContraseñaActual.Size = new Size(300, 32);
             txtContraseñaActual.TabIndex = 1;
             txtContraseñaActual.UseSystemPasswordChar = true;
             // 
             // txtNuevaContraseña
             // 
             txtNuevaContraseña.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtNuevaContraseña.Location = new Point(44, 141);
+            txtNuevaContraseña.Location = new Point(50, 188);
+            txtNuevaContraseña.Margin = new Padding(3, 4, 3, 4);
             txtNuevaContraseña.Name = "txtNuevaContraseña";
-            txtNuevaContraseña.Size = new Size(263, 27);
+            txtNuevaContraseña.Size = new Size(300, 32);
             txtNuevaContraseña.TabIndex = 2;
             txtNuevaContraseña.UseSystemPasswordChar = true;
             // 
             // txtConfirmarContraseña
             // 
             txtConfirmarContraseña.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtConfirmarContraseña.Location = new Point(44, 188);
+            txtConfirmarContraseña.Location = new Point(50, 251);
+            txtConfirmarContraseña.Margin = new Padding(3, 4, 3, 4);
             txtConfirmarContraseña.Name = "txtConfirmarContraseña";
-            txtConfirmarContraseña.Size = new Size(263, 27);
+            txtConfirmarContraseña.Size = new Size(300, 32);
             txtConfirmarContraseña.TabIndex = 3;
             txtConfirmarContraseña.UseSystemPasswordChar = true;
             // 
             // btnCambiarContraseña
             // 
             btnCambiarContraseña.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnCambiarContraseña.Location = new Point(44, 254);
+            btnCambiarContraseña.Location = new Point(50, 339);
+            btnCambiarContraseña.Margin = new Padding(3, 4, 3, 4);
             btnCambiarContraseña.Name = "btnCambiarContraseña";
-            btnCambiarContraseña.Size = new Size(262, 38);
+            btnCambiarContraseña.Size = new Size(299, 51);
             btnCambiarContraseña.TabIndex = 4;
             btnCambiarContraseña.Text = "Cambiar Contraseña";
             btnCambiarContraseña.UseVisualStyleBackColor = true;
@@ -74,9 +79,10 @@
             // btnCancelar
             // 
             btnCancelar.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnCancelar.Location = new Point(44, 300);
+            btnCancelar.Location = new Point(50, 400);
+            btnCancelar.Margin = new Padding(3, 4, 3, 4);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(262, 38);
+            btnCancelar.Size = new Size(299, 51);
             btnCancelar.TabIndex = 5;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = true;
@@ -84,16 +90,17 @@
             // 
             // CambioContraseña
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Brown;
-            ClientSize = new Size(350, 375);
+            ClientSize = new Size(400, 500);
             Controls.Add(txtUsuario);
             Controls.Add(txtContraseñaActual);
             Controls.Add(txtNuevaContraseña);
             Controls.Add(txtConfirmarContraseña);
             Controls.Add(btnCambiarContraseña);
             Controls.Add(btnCancelar);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "CambioContraseña";
             Opacity = 0.95D;
             StartPosition = FormStartPosition.CenterScreen;
--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee49ebe0c3891898f218766c96ccd936ca7d302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8492c196552c70386051105fa1706e32495e283")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
